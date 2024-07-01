50e85afd24a78d1b50f177a87c4154330316fde6
IT22074072 <it22074072@my.sliit.lk>
2024-07-01 23:59:18 +0530

    displaying input value that is being entered

--- epi4/src/App.js
+++ epi4/src/App.js
@@ -1,6 +1,24 @@
 import "./App.css";
-import {useState} from "react";  //to create a state
+import { useState } from "react"; //to create a state(for the state management)
 
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+
+  //event - to grab the information about the input
+  const handleInputChange =(event)=>{
+    setInputValue(event.target.value);
+
+  }
+  return (
+    <div className="app">
+      <input type="text" onChange={handleInputChange}/>
+      {inputValue}
+    </div>
+  );
+}
+
+/*
 function App() {
   const [age, setAge] = useState(0)
   
@@ -17,4 +35,5 @@ function App() {
   );
 }
 
+*/
 export default App;
