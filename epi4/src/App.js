97d384b04814810d111be095b13a055277ac098d
IT22074072 <it22074072@my.sliit.lk>
2024-07-01 23:39:45 +0530

    increment using useState

--- epi4/src/App.js
+++ epi4/src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import {useState} from "react";  //to create a state
 
 function App() {
+  const [age, setAge] = useState(0)
+  
+  const increaseAge = () =>{
+  setAge(age + 1);
+
+  };
+
   return (
-    <div className="App"></div>
+    <div className="app">
+      {age}
+      <button onClick={increaseAge}>Increase Age</button>
+    </div>
   );
 }
 
 export default App;
-
